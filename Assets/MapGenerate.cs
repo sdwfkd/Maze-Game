775c3487218b8ae220f007fca57cbe16685120c0
sdwfkd <sdwfkd@umsystem.edu>
2022-04-27 15:08:33 -0500

    Editing Maze generation

--- Assets/MapGenerate.cs
+++ Assets/MapGenerate.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {	
+	public int mapSize = 50;
 
+	private void Start(){
+		//mapSize = mapSize-1;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// Possible Maze Generation Algorithm Code:
+	// https://weblog.jamisbuck.org/2011/1/12/maze-generation-recursive-division-algorithm
+
+	bool[,] GenerateMap(){
+		bool[,] map = new bool[mapSize, mapSize]; ;
+
+		for(int i = 0; i < mapSize; i++){
+			for(int j = 0; j < mapSize; j++){
+				if(i == 0 || i == mapSize-1){
+					map[i, j] = true;
+				}
+				else{
+					if(j == 0 || j == mapSize-1){
+						map[i, j] = true;
+					}
+					else{
+
+					}
+				}
+			}
+		}
+
+		return map;
 	}
 }
