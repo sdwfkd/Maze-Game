17406b76437a3ff1fa4f9909fb9d9b5c6e09f1a4
Aidan Henry <ajhenry3000@gmail.com>
2022-05-13 11:27:06 -0500

    More complete project
    borders and exit with better generation

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -17,16 +17,42 @@ public class MazeGenerator : MonoBehaviour {
 
 			maze = new bool[width, height];
 
-			recursiveDivision(width, height, 0, 0);
+            int borderwidth = width + 1;
+            int borderheight = height + 1;
+            for (int i = 0; i < width + 1; i++)
+            {
+                var instance = Instantiate(wall);
+                instance.transform.position += new Vector3(i, 0, borderheight);
+            }
+            for (int i = 0; i < borderheight; i++)
+            {
+                var instance = Instantiate(wall);
+                instance.transform.position += new Vector3(borderwidth, 0, i);
+            }
+            for (int i = 2; i < borderheight; i++)
+            {
+                var instance = Instantiate(wall);
+                instance.transform.position += new Vector3(0, 0, i);
+            }
+            for (int i = 0; i < borderheight; i++)
+            {
+                var instance = Instantiate(wall);
+                instance.transform.position += new Vector3(i, 0, 0);
+            }
 
-			var instance = null;
+            recursiveDivision(width, height, 0, 0);
 
-			// Loop through and spawn each wall
-			for (int i = 0; i < width; i++){
+            maze[width / 2, height / 2] = false;
+            maze[0, 2] = false;
+            maze[0, 0] = false;
+            maze[0, 1] = false;
+
+            // Loop through and spawn each wall
+            for (int i = 0; i < width; i++){
 				for (int j = 0; j < height; j++){
 					//Debug.Log("Maze [" + i + ", "+ j +"] = " + maze[i,j]);
-					if (maze[i,j]) {
-						instance = Instantiate(wall);
+					if (maze[i,j] == true) {
+						var instance = Instantiate(wall);
 						instance.transform.position += new Vector3(i, 0, j);
 					}
 				}
@@ -54,48 +80,57 @@ public class MazeGenerator : MonoBehaviour {
 			return;
 		}
 
-		// Choose a random split either horizontal of vertical
-		int horizontal = Random.Range(1,100) % 2;
+        // Choose a random split either horizontal of vertical
+        int horizontal = Random.Range(1,100) % 2;
 		int wall, path;
+        //wall = Random.Range(offsetY, height);
+        //path = Random.Range(offsetX, width);
 
-		if(horizontal == 1){
+        wall = Random.Range(offsetY, height);
+        path = Random.Range(offsetX, width);
+
+        if (horizontal == 1){
 			// Choose where to make the wall, and where the open path will be
-			wall = Random.Range(offsetY, height);
-			path = Random.Range(offsetX, width);
+			//wall = Random.Range(offsetY, height);
+			//path = Random.Range(offsetX, width);
 
 			// Fill them in our maze
 			for(int i = offsetX; i < width; i++){
-				if(i != path){
-					maze[wall, path] = true;
-				}else{
-					maze[wall, path] = false;
-				}
+                //if(i != path){
+                //	maze[wall, path] = true;
+                //}else{
+                //	maze[wall, path] = false;
+                Debug.Log("Maze iteration: [" + wall + ", " + i + "]");
+                maze[wall, i] = true;
 			}
+            maze[wall, path] = false;
 
 			// call recursiveDivision for the top and bottom half
-			recursiveDivision(width - wall, height, offsetX, offsetY);
-			recursiveDivision(width - wall, height, offsetX + wall, offsetY);
+			recursiveDivision(width, (wall - offsetY), offsetX, offsetY);
+			recursiveDivision(width, offsetY + height - wall, offsetX + wall, wall);
 		}
 		else{
 			// Choose where to make the wall, and where the open path will be
-			wall = Random.Range(offsetY, height);
-			path = Random.Range(offsetX, width);
+			//wall = Random.Range(offsetX, width);
+			//path = Random.Range(offsetY, height);
 
 			// Fill them in on our maze
 			for (int i = offsetY; i < height; i++){
-				if (i != path){
-					maze[path, wall] = true;
-				}
-				else{
-					maze[path, wall] = false;
-				}
+                //if (i != path){
+                //	maze[path, wall] = true;
+                //}
+                //else{
+                //	maze[path, wall] = false;
+                //}
+                Debug.Log("Maze iteration: [" + wall + ", " + i + "]");
+                maze[i, wall] = true;
 			}
+            maze[path, wall] = false;
 
 			// call recursiveDivision for the left and right half
-			recursiveDivision(width, height - wall, offsetX, offsetY);
-			recursiveDivision(width, height - wall, offsetX, offsetY + wall);
+			recursiveDivision(wall - offsetX, height, offsetX, offsetY);
+			recursiveDivision(width + offsetX - wall, height - wall, wall, offsetY);
 		}
-
 		Debug.Log("Recursive Div End");
 	}
 }
